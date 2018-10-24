1aade7dddaf260b937009559d6ddc2f05e0620b3
vvedeshkin <evilfunt@gmail.com>
2018-10-24 23:12:57 +0300

    Set up new coordinate system :)

--- core/src/ru/geekbrains/base/Base2DScreen.java
+++ core/src/ru/geekbrains/base/Base2DScreen.java
@@ -51,8 +51,8 @@ public class Base2DScreen implements Screen, InputProcessor {
         screenBounds.setBottom(0);
 
         float aspect = width / (float) height;
-        worldBounds.setHeight(1f);
-        worldBounds.setWidth(1f*aspect);
+        worldBounds.setHeight(42f);
+        worldBounds.setWidth(42f*aspect);
         MatrixUtils.calcTransitionMatrix(worldToGl, worldBounds, glBounds);
         batch.setProjectionMatrix(worldToGl);
         MatrixUtils.calcTransitionMatrix(screenToWorld, screenBounds, worldBounds);
